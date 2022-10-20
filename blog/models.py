da00880df45f32df22b887b014cd423fbde2a8fc
ose0221 <ose0221@naver.com>
2022-10-20 14:31:31 +0900

    포스트 목록/상세 페이지에 author 출력하기

--- blog/models.py
+++ blog/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 import os
 
 # Create your models here.
@@ -14,10 +15,11 @@ class Post(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
-    #추후 outhor 작성
+    #추후 author 작성
+    author = models.ForeignKey(User, null=True, on_delete=models.SET_NULL) #CASCADE - 탈퇴 시, 게시물 삭제
 
     def __str__(self):
-        return f'[{self.pk}]{self.title} : {self.created_at}'
+        return f'[{self.pk}]{self.title}:: {self.author}  : {self.created_at}'
 
     def get_absolute_url(self):
         return f'/blog/{self.pk}/'
