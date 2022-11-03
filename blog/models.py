ee823b79b47267044ae37806769ee9f438407da0
ose0221 <ose0221@naver.com>
2022-11-03 14:44:22 +0900

    다대다 관계 구현하기 완료

--- blog/models.py
+++ blog/models.py
@@ -3,6 +3,16 @@ from django.contrib.auth.models import User
 import os
 
 # Create your models here.
+class Tag(models.Model):
+    name = models.CharField(max_length=50, unique=True)
+    slug = models.SlugField(max_length=200, unique=True, allow_unicode=True)
+
+    def __str__(self):
+        return self.name
+
+    def get_absolute_url(self):
+        return f'/blog/tag/{self.slug}/'
+
 class Category(models.Model):
     name = models.CharField(max_length=50, unique=True)
     slug = models.SlugField(max_length=200, unique=True, allow_unicode=True)
@@ -11,7 +21,7 @@ class Category(models.Model):
         return self.name
 
     def get_absolute_url(self):
-        return f'/blog/category/{self.slug}'
+        return f'/blog/category/{self.slug}/'
 
     class Meta:
         verbose_name_plural = 'Categories'
@@ -32,6 +42,7 @@ class Post(models.Model):
     author = models.ForeignKey(User, null=True, on_delete=models.SET_NULL) #CASCADE - 탈퇴 시, 게시물 삭제
 
     category = models.ForeignKey(Category, null=True, blank=True, on_delete=models.SET_NULL)
+    tags=models.ManyToManyField(Tag, blank=True)
 
     def __str__(self):
         return f'[{self.pk}]{self.title}:: {self.author}  : {self.created_at}'
--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from .models import Post, Category
+from .models import Post, Category, Tag
 from django.views.generic import ListView, DetailView
 
 # Create your views here.
@@ -41,6 +41,20 @@ def category_page(request,slug):
                    'categories' : Category.objects.all(),
                    'no_category_post_count' : Post.objects.filter(category=None).count
                    })
+
+
+def tag_page(request, slug):
+    tag = Tag.objects.get(slug=slug)
+    post_list = tag.post_set.all()
+
+    return render(request, 'blog/post_list.html', {
+        'tag' : tag,
+        'post_list' : post_list,
+        'categories': Category.objects.all(),
+        'no_category_post_count': Post.objects.filter(category=None).count
+
+    })
+
 #def index(request):
 #    posts1 = Post.objects.all().order_by('-pk')
 #    return render(request, 'blog/index.html', {'posts2': posts1}) #오른쪽 posts는 위에서 선언한 posts!
